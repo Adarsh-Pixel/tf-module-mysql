88aadb3d409cdf67829ae92ca8b9506fe65e1765
unknown <kumaradarsh921@gmail.com>
2024-04-14 23:02:32 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,14 +1,15 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  db_name              = "mysql"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin1"
-  password             = "roboshop1"
-  parameter_group_name = aws_db_parameter_group.default.name
-  db_subnet_group_name = aws_db_subnet_group.mysql.name
-  skip_final_snapshot  = true
+  identifier              = "roboshop-${var.ENV}-mysql"
+  allocated_storage       = 10
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t3.micro"
+  username                = "admin1"
+  password                = "roboshop1"
+  parameter_group_name    = aws_db_parameter_group.default.name
+  db_subnet_group_name    = aws_db_subnet_group.mysql.name
+  vpc_security_group_ids  = aws_security_group.allows_mysql.id
+  skip_final_snapshot     = true
 }
 
 resource "aws_db_subnet_group" "mysql" {
