03a5bb3b77d183e09397ebf3b692ec8accf9c5b3
unknown <kumaradarsh921@gmail.com>
2024-04-14 21:54:44 +0530

    Git Auto push

--- cluster.tf
+++ cluster.tf
@@ -1,27 +1,27 @@
-# Creates Elastic Cache cluster
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "roboshop-${var.ENV}-redis"
-  engine               = "redis"
-  node_type            = "cache.t3.small"
-  num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.redis.name
-  subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_ids   = [aws_security_group.allows_redis.id]
-  engine_version       = "6.2"
-  port                 = 6379
-}
+# # Creates Elastic Cache cluster
+# resource "aws_elasticache_cluster" "redis" {
+#   cluster_id           = "roboshop-${var.ENV}-redis"
+#   engine               = "redis"
+#   node_type            = "cache.t3.small"
+#   num_cache_nodes      = 1
+#   parameter_group_name = aws_elasticache_parameter_group.redis.name
+#   subnet_group_name    = aws_elasticache_subnet_group.redis.name
+#   security_group_ids   = [aws_security_group.allows_redis.id]
+#   engine_version       = "6.2"
+#   port                 = 6379
+# }
 
-resource "aws_elasticache_parameter_group" "redis" {
-  name   = "roboshop-${var.ENV}-redis-pg"
-  family = "redis6.x"
-}
+# resource "aws_elasticache_parameter_group" "redis" {
+#   name   = "roboshop-${var.ENV}-redis-pg"
+#   family = "redis6.x"
+# }
 
 
-resource "aws_elasticache_subnet_group" "redis" {
-  name       = "roboshop-${var.ENV}-redis-subnet-group"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-  tags = {
-    Name = "roboshop-${var.ENV}-redis-subnet-group"
-  }
-}
+# resource "aws_elasticache_subnet_group" "redis" {
+#   name       = "roboshop-${var.ENV}-redis-subnet-group"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+#   tags = {
+#     Name = "roboshop-${var.ENV}-redis-subnet-group"
+#   }
+# }
 
--- sg.tf
+++ sg.tf
@@ -1,13 +1,13 @@
 # Creates security group
-resource "aws_security_group" "allows_redis" {
-  name                      = "allows_redis"
+resource "aws_security_group" "allows_mysql" {
+  name                      = "allows_mysql"
   description               = "Allows Only private traffic"
   vpc_id                    = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
-    description              = "redis from private network"
-    from_port                = 6379
-    to_port                  = 6379
+    description              = "mysql from private network"
+    from_port                = 3306
+    to_port                  = 3306
     protocol                 = "tcp"
     cidr_blocks              = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
@@ -20,6 +20,6 @@ resource "aws_security_group" "allows_redis" {
   }
 
   tags = {
-    Name                     = "roboshop-${var.ENV}-redis-sg"
+    Name                     = "roboshop-${var.ENV}-mysql-sg"
   }
 }
