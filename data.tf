1b0f265e1ad3299cf5c6d9808b8fb6378da25ea7
unknown <kumaradarsh921@gmail.com>
2024-04-26 18:38:20 +0530

    Git Auto push

--- data.tf
+++ data.tf
@@ -14,6 +14,6 @@ data "aws_secretsmanager_secret" "secrets" {
 }
 
 # fetches the secrets version from the above server (fetches the secret)
-resource "aws_secretsmanager_secret_version" "secret_version" {
+data "aws_secretsmanager_secret_version" "secret_version" {
   secret_id     = data.aws_secretsmanager_secret.secrets.id
 }
