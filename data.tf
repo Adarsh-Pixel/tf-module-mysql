5f73e642c2145f8ece1947b43ac869c26291b188
unknown <kumaradarsh921@gmail.com>
2024-04-26 18:18:44 +0530

    Git Auto push

--- data.tf
+++ data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+# Fetches the information of the secret (reads the information of secret)
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+# fetches the secrets version from the above server (fetches the secret)
+resource "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id     = data.aws_secretsmanager_secret.secret_version.id
+}
